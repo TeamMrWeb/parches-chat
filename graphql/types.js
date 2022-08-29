80c9514120328e4e593d106c818c1ef6530be986
Manu <mcabral@frba.utn.edu.ar>
2022-08-29 06:41:06 -0300

    feat: add message and chat types

--- graphql/types.js
+++ graphql/types.js
@@ -5,7 +5,12 @@
  */
 
 // required modules
-const { GraphQLObjectType, GraphQLID, GraphQLString } = require('graphql')
+const {
+	GraphQLObjectType,
+	GraphQLID,
+	GraphQLString,
+	GraphQLList,
+} = require('graphql')
 
 // defining types
 const UserType = new GraphQLObjectType({
@@ -34,6 +39,72 @@ const UserType = new GraphQLObjectType({
 	},
 })
 
+const MessageType = new GraphQLObjectType({
+	name: 'MessageType',
+	fields: {
+		id: {
+			type: GraphQLID,
+			description: 'The id of the message.',
+		},
+		text: {
+			type: GraphQLString,
+			description: 'The text of the message.',
+		},
+		image: {
+			type: GraphQLString,
+			description: 'The image of the message.',
+		},
+		author: {
+			type: UserType,
+			description: 'The author of the message.',
+		},
+		seen: {
+			type: new GraphQLList(UserType),
+			description: 'The users that have seen the message.',
+		},
+		updatedAt: {
+			type: GraphQLString,
+			description: 'The date of the last update.',
+		},
+		createdAt: {
+			type: GraphQLString,
+			description: 'The date of the creation.',
+		},
+	},
+})
+
+const ChatType = new GraphQLObjectType({
+	name: 'ChatType',
+	fields: {
+		id: {
+			type: GraphQLID,
+			description: 'The id of the chat.',
+		},
+		name: {
+			type: GraphQLString,
+			description: 'The name of the chat.',
+		},
+		users: {
+			type: new GraphQLList(UserType),
+			description: 'The users of the chat.',
+		},
+		messages: {
+			type: new GraphQLList(MessageType),
+			description: 'The messages of the chat.',
+		},
+		updatedAt: {
+			type: GraphQLString,
+			description: 'The date of the last update.',
+		},
+		createdAt: {
+			type: GraphQLString,
+			description: 'The date of the creation.',
+		},
+	},
+})
+
 module.exports = {
 	UserType,
+	MessageType,
+	ChatType,
 }
