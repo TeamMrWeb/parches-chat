57ab042233096a2a49f4da754f02542eb4f9e52f
Manu <mcabral@frba.utn.edu.ar>
2022-08-29 02:39:30 -0300

    feat: add users query to export

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,9 +1,10 @@
 /**
  * @file Contains all GraphQL queries exports.
  * @author Manuel Cabral
- * @version 0.0.1
+ * @version 0.0.2
  */
 
 module.exports = {
 	hello: require('./queries/hello'),
+	users: require('./queries/users'),
 }
