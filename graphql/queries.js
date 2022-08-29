c402b7903d51b739098b2ae6686fac6d5035c212
Manu <mcabral@frba.utn.edu.ar>
2022-08-29 06:39:49 -0300

    feat: add chat queries

--- graphql/queries.js
+++ graphql/queries.js
@@ -8,4 +8,6 @@ module.exports = {
 	hello: require('./queries/hello'),
 	users: require('./queries/users'),
 	user: require('./queries/user'),
+	chat: require('./queries/chat'),
+	chats: require('./queries/chats'),
 }
