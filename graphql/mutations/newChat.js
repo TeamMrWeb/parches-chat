18fc996977c5f21bd119e056e4aeebc04ac1c005
Manu <mcabral@frba.utn.edu.ar>
2022-08-30 10:10:32 -0300

    refactor: more eff chat create mutation

--- graphql/mutations/newChat.js
+++ graphql/mutations/newChat.js
@@ -1,12 +1,12 @@
 /**
  * @file Contains chat create mutation.
  * @author Manuel Cabral
- * @version 0.0.4
+ * @version 0.0.5
  */
 
 // required modules
 const { createChat } = require('../../controllers/chatController')
-const { findMany } = require('../../controllers/userController')
+const { findMany, findById } = require('../../controllers/userController')
 const { ChatType } = require('../types')
 const {
 	GraphQLNonNull,
@@ -17,8 +17,14 @@ const {
 
 // arguments object
 const args = {
-	name: { type: new GraphQLNonNull(GraphQLString) },
-	users: { type: new GraphQLNonNull(new GraphQLList(GraphQLID)) },
+	name: {
+		type: new GraphQLNonNull(GraphQLString),
+		description: 'The name of the chat.',
+	},
+	usersId: {
+		type: new GraphQLNonNull(new GraphQLList(GraphQLID)),
+		description: 'The id of the users of the chat.',
+	},
 }
 
 /**
@@ -31,10 +37,22 @@ const args = {
 const resolve = async (_, args, context) => {
 	const { user } = context
 	if (!user) throw new Error('You must be logged in to create a chat')
-	const users = await findMany(args.users)
-	if (!users) throw new Error('Invalid users')
+
+	const users = await findMany(args.usersId)
+	const author = await findById(user.id)
+	if (!author) throw new Error('Invalid author id')
+	if (!users) throw new Error('Invalid users id')
+
 	if (users.length < 2) throw new Error('Chat must have at least 2 users')
-	return await createChat(args)
+	const isGroup = users.length > 2
+
+	return await createChat({
+		name: args.name,
+		isGroup,
+		messages: [],
+		admins: isGroup ? [author.id] : [],
+		users: args.usersId,
+	})
 }
 
 // mutation object
