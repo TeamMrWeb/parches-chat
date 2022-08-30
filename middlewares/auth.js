37abf2453f28f9e74c2f9c8496547aa15e157370
Manu <mcabral@frba.utn.edu.ar>
2022-08-30 08:57:46 -0300

    fix: add try catch on auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,27 +1,31 @@
 /**
  * @file Contains authentification middleware.
  * @author Manuel Cabral
- * @version 0.0.2
+ * @version 0.0.3
  */
 
 // required modules
 const jwt = require('jsonwebtoken')
+const { SECRET, EXPIRES_IN } = require('../config').JWT
 
 /**
  * Authenticate token though auth header.
  * @param {Object} req - Express request object.
  * @param {Object} res - Express response object.
  * @param {Function} next - Express next function.
- * @returns {void}
+ * @returns {void} Nothing.
  */
 const authenticate = (req, res, next) => {
 	const token = req.headers.auth
-	if (!token) return res.status(401).json({ error: 'No token provided.' })
-	jwt.verify(token, 'parches123', (err, decoded) => {
-		if (err) res.status(401).json({ error: 'Unauthorized' })
-		else req.user = decoded.user
-	})
-	next()
+	//if (!token) return res.status(401).json({ error: 'No token provided.' })
+	try {
+		const decoded = jwt.verify(token, SECRET)
+		req.user = decoded.user
+		next()
+	} catch (err) {
+		console.error(err)
+		next()
+	}
 }
 
 module.exports = authenticate
