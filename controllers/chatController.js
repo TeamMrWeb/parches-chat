543d6062a778f0552e908eb45a17137e8609d511
Manu <mcabral@frba.utn.edu.ar>
2022-08-30 10:35:03 -0300

    refactor: add findById and remove cast function

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,12 +1,12 @@
 /**
  * @file Contains message model functions.
  * @author Manuel Cabral
- * @version 0.0.2
+ * @version 0.0.3
  */
 
 // required modules
 const { Chat } = require('../models')
-const { ToObjectId } = require('../utils/cast')
+const { ObjectId } = require('mongoose').Types
 
 /**
  *
@@ -43,18 +43,30 @@ const findOne = async (query) => {
 const findAll = async () => await Chat.find()
 
 /**
+ * Find a chat by its id.
+ * @param {String} id - The id of the chat to find.
+ * @returns {Object} The chat found, or null if not found.
+ */
+const findById = async (id) => {
+	if (!ObjectId.isValid(id)) return null
+	return await Chat.findById(id)
+}
+
+/**
  * Add a message to a chat.
- * @param {String} chatId
- * @param {String} messageId
+ * @param {String} chatId - The id of the chat to add the message to.
+ * @param {String} messageId - The id of the message to add to the chat.
+ * @returns {void} Nothing.
  */
 const addMessage = async (chatId, messageId) => {
-	const chat = await findOne({ _id: chatId })
+	const chat = await findById(chatId)
 	chat.messages.push(messageId)
 	await chat.save()
 }
 
 module.exports = {
 	createChat,
+	findById,
 	findOne,
 	findAll,
 	addMessage,
