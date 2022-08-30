b1aaeae288daee8c9aa2eeb42133b4d28db93817
Manu <mcabral@frba.utn.edu.ar>
2022-08-30 10:09:17 -0300

    feat: add findById

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 /**
  * @file Contains user model functions.
  * @author Manuel Cabral
- * @version 0.0.4
+ * @version 0.0.5
  */
 
 // required modules
@@ -30,6 +30,16 @@ const findOne = async (data, secure = true) =>
 	await User.findOne(data).select(secure ? '' : '+password')
 
 /**
+ * Find a user by its id.
+ * @param {String} id - The id of the user to find.
+ * @returns {Object} The user found, or null if not found.
+ */
+const findById = async (id) => {
+	if (!ObjectId.isValid(id)) return null
+	return await User.findById(id)
+}
+
+/**
  * Find all users.
  * @returns {Array} The users found.
  */
@@ -38,15 +48,17 @@ const findAll = async () => await User.find()
 /**
  * Find many users by their ids.
  * @param {Array} ids - The ids of the users to find.
- * @returns {Array} The users found.
+ * @returns {Array} The users found, or null if not found an id.
  */
 const findMany = async (ids) => {
+	for (const id of ids) if (!ObjectId.isValid(id)) return null
 	const mapIds = ids.map((id) => ObjectId(id))
 	return await User.find({ _id: { $in: mapIds } })
 }
 
 module.exports = {
 	createUser,
+	findById,
 	findOne,
 	findAll,
 	findMany,
