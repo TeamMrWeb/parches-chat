feb23ca35b3e07ee8f483818c39d58c3ca0d51f4
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-09-01 08:25:34 -0300

    feat: improve ChatInput and ChatInfoHeader styles

--- src/components/ChatInput/ChatInput.tsx
+++ src/components/ChatInput/ChatInput.tsx
@@ -4,9 +4,11 @@ import emojiIcon from "../../assets/icons/emoji-icon.svg"
 export default function ChatInput() {
   return (
     <section className="chat-input">
-      <img className="chat-input__insert-image" src={plusIcon} alt="Adjuntar Imágen" />
-      <input className="chat-input__input" type="text" placeholder="Enviar mensaje a erickjq10x" />
-      <img className="chat-input__emoji-image" src={emojiIcon} alt="Enviar emoji" />
+      <div className="chat-input-wrapper">
+        <img className="chat-input__tool" src={plusIcon} alt="Adjuntar Imágen" />
+        <input className="chat-input__input" type="text" placeholder="Enviar mensaje a erickjq10x" />
+        <img className="chat-input__tool" src={emojiIcon} alt="Enviar emoji" />
+      </div>
     </section>
   )
 }
