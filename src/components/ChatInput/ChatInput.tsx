c31c1228b5e526dd25162cab5992b5f34585216a
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-09-02 11:05:27 -0300

    feat: give style to messages

--- src/components/ChatInput/ChatInput.tsx
+++ src/components/ChatInput/ChatInput.tsx
@@ -6,7 +6,7 @@ export default function ChatInput() {
     <section className="chat-input">
       <div className="chat-input-wrapper">
         <img className="chat-input__tool" src={plusIcon} alt="Adjuntar Imágen" />
-        <input className="chat-input__input" type="text" placeholder="Enviar mensaje a erickjq10x" />
+        <input className="chat-input__input" type="text" placeholder="Enviar mensaje a erickjq10x" maxLength={2000} />
         <img className="chat-input__tool" src={emojiIcon} alt="Enviar emoji" />
       </div>
     </section>
--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,3 +1,22 @@
+import Message from "../Message/Message"
+
 export default function Messages() {
-  return <section className="messages">Messages</section>
+  return (
+    <section className="messages">
+      <div className="messages-wrapper">
+        <Message messageText="hola" messageHour="21:29" side="right" />
+        <Message
+          messageText="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima voluptatibus sit iste veniam deleniti vitae nemo aliquam perspiciatis exercitationem molestias. Repellendus iusto id harum. Vero eos itaque beatae iste rerum."
+          messageHour="21:29"
+          side="right"
+        />
+        <Message
+          messageText="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima voluptatibus sit iste veniam deleniti vitae nemo aliquam perspiciatis exercitationem molestias. Repellendus iusto id harum. Vero eos itaque beatae iste rerum.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima voluptatibus sit iste veniam deleniti vitae nemo aliquam perspiciatis exercitationem molestias. Repellendus iusto id harum. Vero eos itaque beatae iste rerum.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima voluptatibus sit iste veniam deleniti vitae nemo aliquam perspiciatis exercitationem molestias. Repellendus iusto id harum. Vero eos itaque beatae iste rerum.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima voluptatibus sit iste veniam deleniti vitae nemo aliquam perspiciatis exercitationem molestias. Repellendus iusto id harum. Vero eos itaque beatae iste rerum.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima voluptatibus sit iste veniam deleniti vitae nemo aliquam perspiciatis exercitationem molestias. Repellendus iusto id harum. Vero eos itaque beatae iste rerum.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima voluptatibus sit iste veniam deleniti vitae nemo aliquam perspiciatis exercitationem molestias. Repellendus iusto id harum. Vero eos itaque beatae iste rerum.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima voluptatibus sit iste veniam deleniti vitae nemo aliquam perspiciatis exercitationem molestias. Repellendus iusto id harum. Vero eos itaque beatae iste rerum.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima voluptatibus sit iste veniam deleniti vitae nemo aliquam perspiciatis exercitationem molestias. Repellendus iusto id harum. Vero eos itaque beatae iste rerum.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima voluptatibus sit iste veniam deleniti vitae nemo aliquam perspiciatis exercitationem molestias. Repellendus iusto id harum. Vero eos itaque beatae iste rerum.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima voluptatibus sit iste veniam deleniti vitae nemo aliquam perspiciatis exercitationem molestias. Repellendus iusto id harum. Vero eos itaque beatae iste rerum."
+          messageHour="21:29"
+          side="right"
+        />
+        <Message messageText="hola" messageHour="21:29" side="left" />
+      </div>
+    </section>
+  )
 }
