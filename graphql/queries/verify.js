b787b108f3ce3ecda815eca6048519ba06cf7e0a
Manu <mcabral@frba.utn.edu.ar>
2022-09-06 18:03:53 -0300

    fix: change verify email query

--- graphql/queries/verify.js
+++ graphql/queries/verify.js
@@ -1,7 +1,7 @@
 /**
  * @file Contains the verify query.
  * @author Manuel Cabral
- * @version 0.0.3
+ * @version 0.0.4
  */
 
 // required modules
@@ -9,36 +9,33 @@ const { GraphQLNonNull, GraphQLString } = require('graphql')
 const { verifyToken, findToken } = require('../../utils/auth')
 const findUser = require('../../controllers/userController').findOne
 
-// arguments object
-const args = {
-	token: {
-		description: 'Token to verify',
-		type: new GraphQLNonNull(GraphQLString),
-	},
-}
-
 /**
- * Resolve a token verification.
+ * Resolve a token email verification.
  * @param {Object} _ - Parent object, not used in this case.
- * @param {Object} args - Arguments passed to the mutation.
+ * @param {Object} args - Arguments passed to the mutation, not used in this case.
+ * @param {Object} context - Context object.
  * @returns {String} - A message.
  */
-const resolve = async (_, args) => {
-	const tokenDb = await findToken(args.token)
-	if (!tokenDb) throw new Error('Token expired')
-	const userDecoded = await verifyToken(args.token, { useEmail: true })
+const resolve = async (_, __, context) => {
+	const token = context.headers.authorization
+	if (!token) throw new Error('No se ha proporcionado un token')
+
+	const userDecoded = await verifyToken(token, { useEmail: true })
+	const tokenDb = await findToken(token)
+	if (!tokenDb) throw new Error('El token no pertenece a nuestro sistema')
+
 	const userDb = await findUser({ email: userDecoded.email }, false)
-	if (!userDb) throw new Error('User not found')
+	if (!userDb) throw new Error('Usuario no encontrado')
+
 	userDb.verified = true
 	tokenDb.remove()
 	await userDb.save()
-	return 'Verified'
+	return 'Verificado correctamente'
 }
 
 // query object
 module.exports = {
 	type: GraphQLString,
-	description: 'Verify a user by a token',
-	args,
+	description: 'Verify a user by a token (through headers)',
 	resolve,
 }
