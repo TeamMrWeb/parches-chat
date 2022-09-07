b4a25022ff3da26909cd3d5e904feae6315bdb30
Manu <mcabral@frba.utn.edu.ar>
2022-09-07 19:51:33 -0300

    chore: remove all elements

--- frontend/src/store/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import alertMessageReducer from "../slicers/alertMessageSlice"
-
-export const store = configureStore({
-  reducer: {
-    alertMessage: alertMessageReducer
-  }
-})
