5a3b4e54f781d6e31fd7bd169f4e91c3e5b57200
Manu <mcabral@frba.utn.edu.ar>
2022-09-18 17:02:37 -0300

    refactor: add index.js for each graphql folder

---
+++ src/graphql/types/index.js
@@ -0,0 +1,11 @@
+/**
+ * @file Contains all GraphQL object types.
+ * @author Manuel Cabral
+ * @version 0.0.6
+ */
+
+module.exports = {
+	UserType: require('./userType'),
+	MessageType: require('./messageType'),
+	ChatType: require('./chatType'),
+}
