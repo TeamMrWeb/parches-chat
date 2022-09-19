48401d8006dbf3abf840257fea8d535af5d18a47
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-09-18 22:53:57 -0300

    fix: fix not auth token given after login

--- src/apollo/useComunication.ts
+++ src/apollo/useComunication.ts
@@ -1,16 +1,20 @@
-import { ApolloClient, HttpLink, InMemoryCache, split } from "@apollo/client"
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache, split } from "@apollo/client"
 import { GraphQLWsLink } from "@apollo/client/link/subscriptions"
 import { getMainDefinition } from "@apollo/client/utilities"
 import { createClient } from "graphql-ws"
 
-export const comunication = () => {
-  const auth = localStorage.getItem("auth")
-
+export const useComunication = () => {
   const httpLink = new HttpLink({
-    uri: "http://localhost:4000/graphql",
-    headers: {
-      auth
-    }
+    uri: "http://localhost:4000/graphql"
+  })
+
+  const authLink = new ApolloLink((operation, forward) => {
+    operation.setContext(() => ({
+      headers: {
+        auth: localStorage.getItem("auth")
+      }
+    }))
+    return forward(operation)
   })
 
   const wsLink = new GraphQLWsLink(
@@ -25,7 +29,7 @@ export const comunication = () => {
       return definition.kind === "OperationDefinition" && definition.operation === "subscription"
     },
     wsLink,
-    httpLink
+    authLink.concat(httpLink)
   )
 
   const client = new ApolloClient({
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import { Provider } from "react-redux"
 import { store } from "./store/store"
 import App from "./App"
 import "./scss/index.css"
-import { comunication } from "./apollo/comunication"
+import { useComunication } from "./apollo/useComunication"
 import React from "react"
 
-const { client } = comunication()
+const { client } = useComunication()
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
