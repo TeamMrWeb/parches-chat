788150b980b12ba4e79ebbc46ed88b083eaf1aa5
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-10-06 09:15:40 -0300

    feat: add larger chunks

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: { port: 3000, host: "0.0.0.0" },
+  build: {
+    chunkSizeWarningLimit: 1600
+  }
 })
