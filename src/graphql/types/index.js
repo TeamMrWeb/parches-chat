8d67d13140c9ee1015dbd73299545b411586a1d4
Manu <mcabral@frba.utn.edu.ar>
2022-10-07 21:21:37 -0300

    fix: some file names

---
+++ src/graphql/types/index.js
@@ -0,0 +1,12 @@
+/**
+ * @file Contains all GraphQL object types.
+ * @author Manuel Cabral
+ * @version 0.0.6
+ */
+
+module.exports = {
+	DateTimeType: require('./dateTimeType'),
+	UserType: require('./userType'),
+	MessageType: require('./messageType'),
+	ChatType: require('./chatType'),
+}
---
+++ src/graphql/types/userType.js
@@ -0,0 +1,56 @@
+/**
+ * @file Contains user type.
+ * @author Manuel Cabral
+ * @version 0.0.5
+ */
+
+// required modules
+const DateTimeType = require('./dateTimeType')
+const {
+	GraphQLObjectType,
+	GraphQLID,
+	GraphQLString,
+	GraphQLInt,
+	GraphQLBoolean,
+} = require('graphql')
+
+const UserType = new GraphQLObjectType({
+	name: 'UserType',
+	description: 'The user type.',
+	fields: {
+		id: {
+			type: GraphQLID,
+			description: 'The id of the user.',
+		},
+		username: {
+			type: GraphQLString,
+			description: 'The username of the user.',
+		},
+		email: {
+			type: GraphQLString,
+			description: 'The email of the user.',
+		},
+		avatar: {
+			type: GraphQLString,
+			description: 'The avatar of the user.',
+		},
+		status: {
+			type: GraphQLInt,
+			description: 'The status of the user.',
+		},
+		verified: {
+			type: GraphQLBoolean,
+			description: 'If the user is verified or not.',
+		},
+		updatedAt: {
+			type: DateTimeType,
+			description: 'The date of the last update.',
+		},
+		createdAt: {
+			type: DateTimeType,
+			description: 'The date of the creation.',
+		},
+	},
+})
+
+module.exports = UserType
