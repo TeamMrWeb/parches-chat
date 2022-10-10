404f3bf84be0dd0297e97a39611a74c9b4c3a5a2
Manu <mcabral@frba.utn.edu.ar>
2022-10-10 12:58:08 -0300

    feat: chat avatar controller and route (#25)

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -6,8 +6,10 @@
 
 // required modules
 const fs = require('fs')
-const { PROFILE_AVATAR_FOLDER } = require('../config').CLOUDINARY
-const { findById } = require('./userController')
+const { PROFILE_AVATAR_FOLDER, CHAT_AVATAR_FOLDER } =
+	require('../config').CLOUDINARY
+const findUserById = require('./userController').findById
+const findChatById = require('./chatController').findById
 const { uploadFile, deleteFile } = require('../services/cloudinary')
 
 /**
@@ -24,7 +26,7 @@ const uploadAvatar = async (req, res) => {
 
 	if (!user) return res.status(401).json({ error: 'Unauthorized' })
 	if (!image) return res.status(400).json({ error: 'No image provided' })
-	const userDb = await findById(user.id)
+	const userDb = await findUserById(user.id)
 	if (!userDb) return res.status(404).json({ error: 'User not found' })
 
 	if (userDb.avatar.public_id !== 'avatar/default_avatar') {
@@ -47,6 +49,38 @@ const uploadAvatar = async (req, res) => {
 		.json({ message: 'Avatar uploaded/updated successfully' })
 }
 
+const uploadChatImage = async (req, res) => {
+	if (!req.files) return res.status(400).json({ error: 'No files provided' })
+	const { image } = req.files
+	const { user } = req
+	const { id } = req.params
+	if (!id) return res.status(400).json({ error: 'No chat id provided' })
+	if (!user) return res.status(401).json({ error: 'Unauthorized' })
+	if (!image) return res.status(400).json({ error: 'No image provided' })
+	const userDb = await findUserById(user.id),
+		chatDb = await findChatById(id)
+	if (!userDb || !chatDb)
+		return res.status(404).json({ error: 'User or chat not found' })
+	if (!chatDb.users.includes(userDb._id))
+		return res.status(401).json({ error: 'Your not have access to this chat' })
+	if (chatDb.avatar.public_id !== 'chat/default_chat_image') {
+		console.log('Deleting old chat image from cloudinary')
+		await deleteFile(chatDb.avatar.public_id)
+	}
+	const { public_id, secure_url } = await uploadFile(
+		image.tempFilePath,
+		CHAT_AVATAR_FOLDER
+	)
+	chatDb.avatar = {
+		public_id,
+		secure_url,
+	}
+	await chatDb.save()
+	await fs.unlinkSync(image.tempFilePath)
+	return res.status(200).json({ message: 'Chat image updated successfully' })
+}
+
 module.exports = {
 	uploadAvatar,
+	uploadChatImage,
 }
--- src/routes/upload.routes.js
+++ src/routes/upload.routes.js
@@ -1,18 +1,22 @@
 /**
  * @file Contains upload routes.
  * @author Manuel Cabral
- * @version 0.0.1
+ * @version 0.0.2
  */
 
 const express = require('express')
 const router = express.Router()
 const fileUpload = require('express-fileupload')
-const { uploadAvatar } = require('../controllers/uploadController')
+const {
+	uploadAvatar,
+	uploadChatImage,
+} = require('../controllers/uploadController')
 
 /**
  * Uploads a file to the server, then uploads it to cloudinary.
  * NOTE: Only accepts images for now.
  */
+
 router.post(
 	'/avatar',
 	fileUpload({
@@ -20,6 +24,15 @@ router.post(
 		tempFileDir: './tmp',
 	}),
 	uploadAvatar
+)
+
+router.post(
+	'/chatimage/:id',
+	fileUpload({
+		useTempFiles: true,
+		tempFileDir: './tmp',
+	}),
+	uploadChatImage
 )
 
 module.exports = router
