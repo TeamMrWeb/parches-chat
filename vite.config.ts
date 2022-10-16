827ea977ddb790fd4a71da315c40455b6edd93d5
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-10-16 18:37:02 -0300

    fix: move base

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+  base: "/parches-chat/",
   plugins: [react()],
   server: { port: 3000, host: "0.0.0.0" },
   build: {
-    chunkSizeWarningLimit: 1600,
-    base: "/parches-chat/"
+    chunkSizeWarningLimit: 1600
   }
 })
