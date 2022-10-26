e627ddbaf7f703db5fb566b9d018b8b71a1c8ccd
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-10-26 18:26:00 -0300

    refactor: use correct types on useChatInput

--- src/components/ChatInput/useChatInput.ts
+++ src/components/ChatInput/useChatInput.ts
@@ -1,47 +1,32 @@
-import { useState } from "react"
+import { FormEvent, useState } from "react"
 import { useFetchingMethod } from "../../apollo/useFetchingMethod"
 import { createMessage } from "../../graphql/mutations"
+import { ChatProps } from "../../ts/interfaces"
 
-interface chat {
-  id?: string
-  name?: string
-  messages?: Array<object>
-  isGroup?: boolean
-  secure?: boolean
-  private?: boolean
-  maxUsers?: number
-  ownerId?: any
-  admins?: Array<object>
-  users?: Array<object>
-  createdAt?: Date
-  updatedAt?: Date
-}
-
-export const useChatInput = (chat: chat) => {
+export const useChatInput = (chat: ChatProps) => {
   const [value, setValue] = useState("")
   const [image, setImage] = useState("")
   const [showEmojisPicker, setShowEmojisPicker] = useState(false)
   const { lazyQueryMethod: createNewMessage } = useFetchingMethod(createMessage)
 
-  window.onclick = (event: any) => {
+  window.onclick = (event: MouseEvent) => {
     if (
       showEmojisPicker &&
-      !document.getElementsByClassName("emoji-picker-react")[0].contains(event.target) &&
-      event.target.className !== "chat-input__tool chat-input__tool--emoji-picker"
+      !document.getElementsByClassName("emoji-picker-react")[0].contains(event.target as Element) &&
+      (event.target as Element).className !== "chat-input__tool chat-input__tool--emoji-picker"
     )
       setShowEmojisPicker(false)
   }
 
-  const previewImage = (imageRoute: any) => {
+  const previewImage = (imageRoute: File) => {
     const route = URL.createObjectURL(imageRoute)
     setImage(route)
   }
 
-  const submitMessage = (event: any) => {
+  const submitMessage = (event: FormEvent) => {
     event.preventDefault()
     const message = value
     const chatId = chat.id
-    console.log(message)
     createNewMessage({ variables: { chatId: chatId, text: message } })
     setValue("")
   }
