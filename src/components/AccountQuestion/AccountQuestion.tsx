a8b7d4367d53e8c6a27b84ca51570a99b71b319a
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-10-26 19:01:45 -0300

    refactor: move AccountQuestion interface

--- src/components/AccountQuestion/AccountQuestion.tsx
+++ src/components/AccountQuestion/AccountQuestion.tsx
@@ -1,12 +1,7 @@
 import { Link } from "react-router-dom"
+import { AccountQuestionProps } from "../../ts/interfaces"
 
-interface AccountQuestion {
-  question: string
-  href: string
-  hrefText: string
-}
-
-export default function AccountQuestion({ question, href, hrefText }: AccountQuestion) {
+export default function AccountQuestion({ question, href, hrefText }: AccountQuestionProps) {
   return (
     <div className="account-question">
       <p className="account-question__text">{question}</p>
