d0add8814f15aec87515d5d85e007f208284bf73
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-10-29 16:11:36 -0300

    feat: add flood protection on chat

--- src/components/ChatInput/useChatInput.ts
+++ src/components/ChatInput/useChatInput.ts
@@ -1,6 +1,8 @@
-import { FormEvent, useState } from "react"
+import { FormEvent, useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
 import { useFetchingMethod } from "../../apollo/useFetchingMethod"
 import { createMessage } from "../../graphql/mutations"
+import { createAlertMessage } from "../../slicers/alertMessageSlice"
 import { ChatProps } from "../../ts/interfaces"
 
 export const useChatInput = (chat: ChatProps) => {
@@ -8,6 +10,17 @@ export const useChatInput = (chat: ChatProps) => {
   const [image, setImage] = useState("")
   const [showEmojisPicker, setShowEmojisPicker] = useState(false)
   const { lazyQueryMethod: createNewMessage } = useFetchingMethod(createMessage)
+  const [messageDelay, setMessageDelay] = useState(new Date().getSeconds())
+  const [chatIsTimedOut, setChatIsTimedOut] = useState(false)
+  const [canWrite, setCanWrite] = useState(true)
+  const [messagesIn5s, setMessagesIn5s] = useState(0)
+  const [chatMessageStarted, setChatMessageStarted] = useState(false)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const timestamp = new Date().getSeconds()
+    if (timestamp === messageDelay) setMessageDelay(timestamp)
+  }, [messageDelay])
 
   window.onclick = (event: MouseEvent) => {
     if (
@@ -23,13 +36,55 @@ export const useChatInput = (chat: ChatProps) => {
     setImage(route)
   }
 
+  const protectFromFlood = () => {
+    if (messagesIn5s > 6 && canWrite) {
+      setCanWrite(false)
+      setChatIsTimedOut(true)
+      setTimeout(() => {
+        setCanWrite(true)
+        setChatIsTimedOut(false)
+        setMessagesIn5s(0)
+      }, 5000)
+    } else if (canWrite) {
+      setMessagesIn5s(messagesIn5s + 1)
+      if (!chatMessageStarted) {
+        setChatMessageStarted(true)
+        setTimeout(() => {
+          setChatMessageStarted(false)
+          setMessagesIn5s(0)
+        }, 5000)
+      }
+    }
+  }
+
   const submitMessage = (event: FormEvent) => {
     event.preventDefault()
     const message = value
+    if (message.length <= 0) return
+    protectFromFlood()
+    if (chatIsTimedOut) {
+      return dispatch(
+        createAlertMessage({
+          title: `¡Ey!`,
+          description: "Estas enviando mensajes muy rápido, espera un poco",
+          type: "warning",
+          visible: true
+        })
+      )
+    }
     const chatId = chat.id
     createNewMessage({ variables: { chatId: chatId, text: message } })
     setValue("")
   }
 
-  return { showEmojisPicker, setShowEmojisPicker, previewImage, image, setImage, value, setValue, submitMessage }
+  return {
+    showEmojisPicker,
+    setShowEmojisPicker,
+    previewImage,
+    image,
+    setImage,
+    value,
+    setValue,
+    submitMessage
+  }
 }
