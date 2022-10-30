21d5fcab286aad4b1275a21f1ad69a4c3e4cb8f1
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-10-29 22:44:01 -0300

    feat: add show/hidden password

--- src/components/FormGroupInput/FormGroupInput.tsx
+++ src/components/FormGroupInput/FormGroupInput.tsx
@@ -1,5 +1,8 @@
 import { ErrorMessage, Field } from "formik"
 import { FormGroupProps } from "../../ts/interfaces"
+import { useState } from "react"
+import eyeIcon from "../../assets/icons/eye-icon.svg"
+import closedEyeIcon from "../../assets/icons/closed-eye-icon.svg"
 
 export default function FormGroupInput({
   type,
@@ -8,20 +11,40 @@ export default function FormGroupInput({
   minLength,
   maxLength
 }: FormGroupProps) {
+  const [showPassword, setShowPassword] = useState(false)
+
   return (
     <div className="form-group">
       <label className="form__label" htmlFor={type}>
         {label}
       </label>
-      <Field
-        className="form__input"
-        type={type}
-        name={type}
-        id={type}
-        placeholder={placeholder}
-        minLength={minLength}
-        maxLength={maxLength}
-      />
+      <div className="input-wrapper">
+        <Field
+          className="form__input"
+          type={type === "password" ? (showPassword ? "text" : type) : type}
+          name={type}
+          id={type}
+          placeholder={placeholder}
+          minLength={minLength}
+          maxLength={maxLength}
+        ></Field>
+        {type === "password" &&
+          (showPassword ? (
+            <img
+              className="form__eye-icon"
+              src={closedEyeIcon}
+              alt="Ver contraseña"
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          ) : (
+            <img
+              className="form__eye-icon"
+              src={eyeIcon}
+              alt="Ocultar contraseña"
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          ))}
+      </div>
       <ErrorMessage name={type} />
     </div>
   )
