1f37d558137cdee9790b2dc29b72491952211b8e
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-11-03 07:44:08 -0300

    feat: add back to home button

--- src/sections/NotFound/NotFound.tsx
+++ src/sections/NotFound/NotFound.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom"
+
 export default function NotFound() {
   return (
     <section className="not-found">
       <h1 className="not-found__title">¡Ups! La ruta que has ingresado no existe.</h1>
-      <button className="not-found__button">Volver al inicio</button>
+      <Link className="not-found__button" to="/">
+        Volver al inicio
+      </Link>
     </section>
   )
 }
