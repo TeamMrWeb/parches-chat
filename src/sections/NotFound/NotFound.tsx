99cf734dd90743cfa70f6cb46ca33fef9eac1578
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-11-03 07:42:54 -0300

    feat: add NotFound styles

--- src/sections/NotFound/NotFound.tsx
+++ src/sections/NotFound/NotFound.tsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
     <section className="not-found">
-      <h1 className="not-found__title">¡Ups! Ha ocurrido un error inesperado...</h1>
-      <p className="not-found__error">Error: </p>
+      <h1 className="not-found__title">¡Ups! La ruta que has ingresado no existe.</h1>
+      <button className="not-found__button">Volver al inicio</button>
     </section>
   )
 }
