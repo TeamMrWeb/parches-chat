8c3542b3e606c6c3c76aece4ab26fc1c8eae83dd
leoarayav <leandro.araya08@inacapmail.cl>
2022-11-12 14:16:48 -0300

    style: edited api response text.

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,14 +1,14 @@
 /**
  * @file Contains index routes.
  * @author Manuel Cabral
- * @version 0.0.1
+ * @version 0.0.2
  */
 
 const express = require('express')
 const router = express.Router()
 
 router.get('/', (_, res) => {
-	res.send('Parches Chat API')
+	res.send('Welcome to parches-chat API from backend development :-)')
 })
 
 module.exports = router
