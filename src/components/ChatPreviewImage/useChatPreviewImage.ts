d1bb877309f9c3e606618869beec05e20b80d175
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2022-11-29 08:54:32 -0300

    feat(chat): add send images

--- src/components/ChatPreviewImage/useChatPreviewImage.ts
+++ src/components/ChatPreviewImage/useChatPreviewImage.ts
@@ -1,12 +1,8 @@
-import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useFetchingMethod } from "../../apollo/useFetchingMethod"
 import { createMessage } from "../../graphql/mutations"
 import { createAlertMessage } from "../../slicers/alertMessageSlice"
 import { RootState } from "../../ts/interfaces"
-
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjM2MmIxMmYyMGU1Y2E5ZTVlNTBiYWIwIiwidXNlcm5hbWUiOiJHYWJyaWVsIENhcmFtw6lzIiwiZW1haWwiOiJnYWJyaWVsY2FyYW1lczFAZ21haWwuY29tIn0sImlhdCI6MTY2NzgyMDI3MywiZXhwIjoxNjY3OTA2NjczfQ.KdJnu-r160yvroHzi81eHkxxFfwbMdDlP_VqNbM16Lc"
 
 export const useChatPreviewImage = (
   images: File[],
@@ -17,8 +13,7 @@ export const useChatPreviewImage = (
   const dispatch = useDispatch()
 
   const sendImages = (images: File[], text: string = "a") => {
-    const uploadImageUrl = import.meta.env.VITE_UPLOAD_IMAGE
-    console.log(images)
+    const imageUploadUrl = import.meta.env.VITE_UPLOAD_IMAGE
     images.forEach(async image => {
       const messageCreated = await createNewMessage({
         variables: { chatId: chat.id, text, hasImage: true }
@@ -26,12 +21,11 @@ export const useChatPreviewImage = (
       const messageId = messageCreated.data.createMessage.id
       const chatId = chat.id
       let formData = new FormData()
-      formData.append("image", image, image.name)
-      fetch(`${uploadImageUrl}${chatId}/${messageId}`, {
+      formData.append("image", image)
+      fetch(`${imageUploadUrl}${chatId}/${messageId}`, {
         method: "POST",
         headers: {
-          auth: token,
-          "Content-Type": "multipart/form-data"
+          auth: localStorage.getItem("auth")!
         },
         body: formData
       })
@@ -49,19 +43,6 @@ export const useChatPreviewImage = (
           )
         })
     })
-  }
-
-  const covertFileImageToJSON = (image: File) => {
-    const JSONImage = {
-      image: {
-        lastModified: image.lastModified,
-        name: image.name,
-        size: image.size,
-        type: image.type,
-        webkitRelativePath: image.webkitRelativePath
-      }
-    }
-    return JSON.stringify(JSONImage)
   }
 
   const removeImage = (imageId: number) => {
